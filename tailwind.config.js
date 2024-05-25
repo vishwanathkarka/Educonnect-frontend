90eb9fbe79502f2405ed865ca9a5a0b797087f2c
Vishwanath Reddy Karka <86046138+vishwanathkarka@users.noreply.github.com>
2024-05-25 11:37:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     colors: {
      // primarycolor:"#6B62FF",
-      primarylight:"#6b62ff2e",
+     primarycolor:"#4db24c",
       darkblack:"#1A1E23]",
       lightwg:"#dcdcdc",
       primarylight:"#6b62ff2e",
